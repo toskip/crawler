ce97f73a89fe1e7cf930293a7cf1ace7b151943b
0sys0 <rain_sys@126.com>
2018-12-08 19:37:31 +0800

    finish local version

--- iwara.py
+++ iwara.py
@@ -1,59 +1,77 @@
 from requests_html import HTMLSession
 from threading import Thread
 from queue import Queue
+import codecs
 import time
-def listdownload(threadnum,listqueue,contentqueue):
-    while True:
+import json
+import locale
+def listdownload(session,threadnum,listqueue,contentqueue):
+    while not listqueue.empty():
+        url = listqueue.get(True)
+        try:
+            proxy_dict = {
+                "http": "http://127.0.0.1:1080/",
+                "https": "http://127.0.0.1:1080/"
+            }
+            headers = {
+                'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3',
+            }
+            i = url.split("=")[-1]
+            #r = session.get(url, proxies=proxy_dict)
+            r = session.get(url)
+            content = r.html.find("h3 > a")
+            for each in content:
+               for link in each.absolute_links:
+                   contentqueue.put(link)
+            print(f"线程{threadnum}: 第{i}页处理完毕，共{len(content)}条视频")
+            r.close()
+        except Exception as e:
+            print(e)
+            listqueue.put(url)
 
-        url = listqueue.get(False)
-        session = HTMLSession()
+def contentdownload(session,threadnum,contentqueue,f):
+    while not contentqueue.empty():
+        url = contentqueue.get(True)
         proxy_dict = {
             "http": "http://127.0.0.1:1080/",
             "https": "http://127.0.0.1:1080/"
         }
         headers = {
-            'User-Agent': r'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+            'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3',
         }
-        i = url.split("=")[-1]
-        r = session.get(url, proxies=proxy_dict)
-
-        content = r.html.find(".field-items > div > a")
-        for each in content:
-           for link in each.absolute_links:
-               contentqueue.put(link)
-        print(f"线程{threadnum}: 处理完毕{i}")
-        r.close()
-
-def contentdownload(threadnum,contentqueue,f):
-    while True:
-        url = contentqueue.get(False)
-        session = HTMLSession()
-        proxy_dict = {
-            "http": "http://127.0.0.1:1080/",
-            "https": "http://127.0.0.1:1080/"
-        }
-        headers = {
-            'User-Agent': r'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
-        }
-        r = session.get(url, proxies=proxy_dict)
-        json = {}
-        json["content"] = r.html.find(".field-items > div > a").text()
-        f.write(json)
-        print(f"线程{threadnum}:")
+        #r = session.get(url, proxies=proxy_dict)
+        r = session.get(url)
+        info = r.html.find("div.node-info", first=True)
+        data = {}
+        data["title"] = info.find("h1.title",first=True).text.strip().split("\n作成者:")[0]
+        data["username"] =  info.find("a.username",first=True).text.strip().split(" 作成日:")[0]
+        data["userpage"] = "https://ecchi.iwara.tv"+info.find(".username",first=True).attrs["href"].strip()
+        data["userpicture"] = "https:"+info.find(".user-picture img",first=True).attrs["src"].strip()
+        data["time"] = info.find(".submitted",first=True).text.strip().split("作成日:")[-1]
+        data["description"] = info.find(".field-type-text-with-summary",first=True).text.strip() if info.find(".field-type-text-with-summary",first=True) else ""
+        data["tags"] = info.find(".field-name-field-categories",first=True).text.strip().split('\n')
+        data["like"],data["view"] = info.find(".node-views",first=True).text.strip().split(' ')
+        data["comment"] = r.html.find("#comments > .title",first=True).text.strip().split(' ')[-1] if r.html.find("#comments > .title",first=True) else "0"
+        data["like"] = locale.atoi(data["like"])
+        data["view"] = locale.atoi(data["view"])
+        data["comment"] = locale.atoi(data["comment"])
+        f.write(json.dumps(data,ensure_ascii=False)+'\n')
+        print(f"线程{threadnum}:视频{data['title']}处理完毕")
         r.close()
 
 if __name__ == '__main__':
-
+    session = HTMLSession()
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     listqueue = Queue()
     contentqueue = Queue()
-    for i in range(814):
+    for i in range(12):
         listqueue.put(f"https://ecchi.iwara.tv/videos?page={i}")
-    threadnum = 4  # 随便设
+    threadnum = 6  # 随便设
     t = []
     starttime = time.time()
     print('开始下载列表页,线程数%d' % (threadnum))
     for i in range(threadnum):
-        thread = Thread(target=listdownload, args=(i, listqueue,contentqueue))
+        thread = Thread(target=listdownload, args=(session,i, listqueue,contentqueue))
         thread.start()
         t.append(thread)
         print('线程%d已启动' % (i))
@@ -62,21 +80,19 @@ if __name__ == '__main__':
     print('下载列表页完成')
     endtime = time.time()
     print('用时 %f' % (endtime - starttime))
-    """
-    
-    f = open("temp.txt","w")
+
+
+    f = codecs.open("temp.txt","w","utf-8")
     starttime = time.time()
-    t2 = []
-    print('开始下载内容页,线程数%d' % (threadnum))
+    print('开始下载视频页,线程数%d' % (threadnum))
     for i in range(threadnum):
-        thread = Thread(target=contentdownload, args=(i,contentqueue,f))
-        thread.start()
-        t2.append(thread)
+        t[i] = Thread(target=contentdownload, args=(session,i,contentqueue,f))
+        t[i].start()
         print('线程%d已启动' % (i))
-    for thread in t2:
+    for thread in t:
         thread.join()
-    print('下载内容页完成')
+    print('下载视频页完成')
     endtime = time.time()
     print('用时 %f' % (endtime - starttime))
     f.close()
-    """
+
