cd660754932fc2e5e514e8702c610a13c0dc5a80
0sys0 <rain_sys@126.com>
2018-12-09 17:10:28 +0800

    add thumbnail and url

--- iwara.py
+++ iwara.py
@@ -8,30 +8,30 @@ import locale
 def listdownload(session,threadnum,listqueue,contentqueue):
     while not listqueue.empty():
         url = listqueue.get(True)
-        try:
-            proxy_dict = {
-                "http": "http://127.0.0.1:1080/",
-                "https": "http://127.0.0.1:1080/"
-            }
-            headers = {
-                'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3',
-            }
-            i = url.split("=")[-1]
-            #r = session.get(url, proxies=proxy_dict)
-            r = session.get(url)
-            content = r.html.find("h3 > a")
-            for each in content:
-               for link in each.absolute_links:
-                   contentqueue.put(link)
-            print(f"线程{threadnum}: 第{i}页处理完毕，共{len(content)}条视频")
-            r.close()
-        except Exception as e:
-            print(e)
-            listqueue.put(url)
+        proxy_dict = {
+            "http": "http://127.0.0.1:1080/",
+            "https": "http://127.0.0.1:1080/"
+        }
+        headers = {
+            'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3',
+        }
+        i = url.split("=")[-1]
+        #r = session.get(url, proxies=proxy_dict)
+        r = session.get(url)
+        #r.html.render()
+        content = r.html.find(".node-video")
+        for each in content:
+            temp = []
+            for link in each.find("h3.title",first=True).absolute_links:
+                temp.append(link)
+            temp.append("https:"+each.find("img",first=True).attrs["src"] if each.find("img",first=True) else "")
+            contentqueue.put(temp)
+        print(f"线程{threadnum}: 第{i}页处理完毕，共{len(content)}条视频")
+        r.close()
 
 def contentdownload(session,threadnum,contentqueue,f):
     while not contentqueue.empty():
-        url = contentqueue.get(True)
+        url,thumbnail = contentqueue.get(True)
         proxy_dict = {
             "http": "http://127.0.0.1:1080/",
             "https": "http://127.0.0.1:1080/"
@@ -44,6 +44,8 @@ def contentdownload(session,threadnum,contentqueue,f):
         info = r.html.find("div.node-info", first=True)
         data = {}
         data["title"] = info.find("h1.title",first=True).text.strip().split("\n作成者:")[0]
+        data["url"] = url
+        data["thumbnail"] = thumbnail
         data["username"] =  info.find("a.username",first=True).text.strip().split(" 作成日:")[0]
         data["userpage"] = "https://ecchi.iwara.tv"+info.find(".username",first=True).attrs["href"].strip()
         data["userpicture"] = "https:"+info.find(".user-picture img",first=True).attrs["src"].strip()
@@ -55,6 +57,7 @@ def contentdownload(session,threadnum,contentqueue,f):
         data["like"] = locale.atoi(data["like"])
         data["view"] = locale.atoi(data["view"])
         data["comment"] = locale.atoi(data["comment"])
+
         f.write(json.dumps(data,ensure_ascii=False)+'\n')
         print(f"线程{threadnum}:视频{data['title']}处理完毕")
         r.close()
@@ -64,7 +67,7 @@ if __name__ == '__main__':
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     listqueue = Queue()
     contentqueue = Queue()
-    for i in range(12):
+    for i in range(2):
         listqueue.put(f"https://ecchi.iwara.tv/videos?page={i}")
     threadnum = 6  # 随便设
     t = []
