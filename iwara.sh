05a7e97902b13b7673b354805665f7df3a25d165
nemosc <rain_sys@126.com>
2021-02-26 13:42:10 +0800

    Update iwara.sh

--- iwara.sh
+++ iwara.sh
@@ -21,19 +21,19 @@ grep -oE '<h3 class="title"><a href="(.*)"'  * | awk -F\" '{print $4}' > ./conte
 
 #详情页
 cd content
-for i in {0..14288}
+for i in {0..7145}
 do
   date +'%Y-%m-%d %H:%M:%S'
   echo start batch $i
-  for j in {0..4}
+  for j in {0..9}
   do
-    k=$((i*5+j))
+    k=$((i*10+j))
     url=https://ecchi.iwara.tv$(sed -n "${k},${k}p" seed.txt)
     echo $url
     curl -s -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36' -O $url &
   done
   wait
-  #抓太快会封一个小时ip
+  #抓太快会封一个小时ip，具体阈值没测过
   sleep 5
   echo finish batch $i
 done
